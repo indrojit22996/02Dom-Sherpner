f7fd68f0f57fb583cb65cb9bd8d86f612448cfea
indrojit22996 <indrojit22996bhaduri@gmail.com>
2023-03-24 21:03:15 +0530

    Make the 2nd item have green background color Make the 3rd item invisible

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let listItem = document.getElementsByClassName("list-group-item");
-listItem[2].style.backgroundColor = "green";
+// listItem[2].style.backgroundColor = "green";
 
 for (let i = 0; i < listItem.length; i++) {
   listItem[i].style.color = "#245953";
@@ -13,3 +13,6 @@ li.style.listStyle = "none";
 li.style.fontSize = "18px";
 // document.body.appendChild(li);
 document.getElementById("items").appendChild(li);
+
+listItem[1].style.backgroundColor = "green";
+listItem[2].style.display = "none";
