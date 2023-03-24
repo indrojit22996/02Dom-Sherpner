31e38140bc4178a7dee4251fbf52d103a4dd0b00
indrojit22996 <indrojit22996bhaduri@gmail.com>
2023-03-24 20:55:59 +0530

    append new li eliment with anathor class name

--- script.js
+++ script.js
@@ -5,3 +5,11 @@ for (let i = 0; i < listItem.length; i++) {
   listItem[i].style.color = "#245953";
   listItem[i].style.fontWeight = "800";
 }
+let list = document.querySelector("list-group");
+let li = document.createElement("li");
+li.className = "li-eliment-new";
+li.textContent = "Indrojit";
+li.style.listStyle = "none";
+li.style.fontSize = "18px";
+// document.body.appendChild(li);
+document.getElementById("items").appendChild(li);
